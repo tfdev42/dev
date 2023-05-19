ff16280d963e01dc20fef8631d41b3362f2ddd26
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-19 21:06:39 +0200

    dt

---
+++ projects/java/eclipse/exercises/src/more/Main.java
@@ -0,0 +1,21 @@
+package more;
+import more.PositiveNegativeZero;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*OtherClass otherObject = new OtherClass();
+        otherObject.someMethod();*/
+		
+		//PositiveNegativeZero positiveNegativeZero = new PositiveNegativeZero();
+		
+		
+		
+		int number = 15;
+		
+		
+		PositiveNegativeZero.checkNumber(number);
+
+	}
+
+}
---
+++ projects/java/eclipse/exercises/src/more/PositiveNegativeZero.java
@@ -0,0 +1,18 @@
+package more;
+
+public class PositiveNegativeZero {
+		
+	public static void checkNumber(int number) {
+		
+		if (number == 0) {
+			System.out.println("0");
+		}else if (number > 0) {
+			System.out.println("Positive");
+		}
+		else System.out.println("Negative");		
+		
+	}
+	
+	
+
+}
