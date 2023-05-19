1cc3bcbe6f5b5b92e0d6b0fbbdb6db79a09dbf63
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-19 18:07:35 +0200

    dt

--- projects/java/eclipse/exercises/src/ifelse/IfElseChallenge.java
+++ projects/java/eclipse/exercises/src/ifelse/IfElseChallenge.java
@@ -8,14 +8,34 @@ public class IfElseChallenge {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score < 5000 && score > 1000) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000) {
-            System.out.println("Your score was less than 1000");
-        } else {
-            System.out.println("Got here");
+        int finalScore = score;
+        
+        calculateScore();
+        
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
+        
+        if (gameOver) {
+        	finalScore += (levelCompleted * bonus);
+        	System.out.println("Your final score was " + finalScore);
         }
 
+	}
+	
+	public static void calculateScore() {
+		boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+        
+        if (gameOver) {
+        	finalScore += (levelCompleted * bonus);
+        	System.out.println("Your final score was " + finalScore);
+        }
 	}
 
 }
