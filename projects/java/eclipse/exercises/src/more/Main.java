3f040f1527d9efc966929d8a70829f8af8f7fb49
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-20 13:26:34 +0200

    dt

--- projects/java/eclipse/exercises/src/more/Main.java
+++ projects/java/eclipse/exercises/src/more/Main.java
@@ -1,5 +1,6 @@
 package more;
 import more.PositiveNegativeZero;
+import more.PoNeZe;
 
 public class Main {
 
@@ -11,10 +12,14 @@ public class Main {
 		
 		
 		
-		int number = 15;
+		int number = 0;
 		
 		
-		PositiveNegativeZero.checkNumber(number);
+		PositiveNegativeZero.checkNumber(number); // can't integrate as String because method is void
+		
+		System.out.printf("The Number %d is " + (PoNeZe.checkNumber(number)) + "\n", number); // can integrate because method is String = OK
+		
+		
 
 	}
 
