6bb9c89401cabd9084ff62b45ae0216bb04a39e5
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-21 14:23:33 +0200

    dt

--- projects/java/eclipse/exercises/src/more/Main.java
+++ projects/java/eclipse/exercises/src/more/Main.java
@@ -7,30 +7,27 @@ public class Main {
 	public static void main(String[] args) {
 		/*OtherClass otherObject = new OtherClass();
         otherObject.someMethod();*/
-		
-		//PositiveNegativeZero positiveNegativeZero = new PositiveNegativeZero();
-		
-		
-		
 		int num = 0;
 		
 		System.out.printf("The Number %d is ", num);
 		PositiveNegativeZero.checkNumber(num); // can't integrate directly as String in Print because method is void
-
-
 		// PositiveNegativeZero positiveNegativeZero = new PositiveNegativeZero();
 		// positiveNegativeZero.checkNumber(num);
 		// System.out.println("positiveNegativeZero: " + positiveNegativeZero);
 
 		// String checkedNumber = "" + positiveNegativeZero;
 		// System.out.println(checkedNumber);
-
-
-
-		
 		System.out.printf("The Number %d is " + (PoNeZe.checkNumber(num)) + "\n", num); // can integrate because method is String = OK
 		
 		
+
+		// ***************************************************
+
+		System.out.println("Speedconverter");
+		System.out.println(SpeedConverter.toMilesPerHour(75.114));
+
+
+
 
 	}
 
