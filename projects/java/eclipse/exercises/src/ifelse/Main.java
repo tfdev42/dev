9e90aac08dc7d5bb822fbf50cecbd7e4856899bd
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-24 18:42:16 +0200

    nb

--- projects/java/eclipse/exercises/src/ifelse/Main.java
+++ projects/java/eclipse/exercises/src/ifelse/Main.java
@@ -3,10 +3,10 @@ package ifelse;
 public class Main {
 
 	public static void main(String[] args) {
-		boolean gameOver = true;
+		//boolean gameOver = true;
         int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        //int levelCompleted = 5;
+        //int bonus = 100;
 
         if (score < 5000 && score > 1000) {
             System.out.println("Your score was less than 5000 but greater than 1000");
