505623dde21605184393d456ef3539a7ff48eec6
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-05-26 21:51:15 +0200

    dt

--- projects/java/eclipse/exercises/src/more/TreeMultipl.java
+++ projects/java/eclipse/exercises/src/more/TreeMultipl.java
@@ -8,9 +8,10 @@ class TreeMultipl {
         for (int i=1; i<=10; i++){
             int result = i * multiplicant;
             System.out.printf("%d x %d = ", i, multiplicant);
-            for(int n=1; n<=i; n++){
-                System.out.printf("%d + ", multiplicant);
-            }
+            if (i > 1){ //TODO: give out the correct number of multiplications
+                for(int n=1; n<=i; n++){
+                    System.out.printf("%d + ", multiplicant);
+                };           }
             System.out.printf("%d = %d%n", multiplicant, result);
         }
     }
