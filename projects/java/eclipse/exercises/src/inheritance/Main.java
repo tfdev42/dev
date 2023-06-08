21ae46f079da97805f673e0dfd910d5986acb0d9
tfdev42 <tamas.fazekas.hu@gmail.com>
2023-06-08 11:41:11 +0200

    dt

--- projects/java/eclipse/exercises/src/inheritance/Main.java
+++ projects/java/eclipse/exercises/src/inheritance/Main.java
@@ -3,5 +3,11 @@ package inheritance;
 public class Main {
     public static void main(String[] args) {
         
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Golden Retreaver", 8, 20, 2, 4, 1, 20, "long silky");
+        dog.eat(); // method from Super Class Animal
+
+
     }
 }
